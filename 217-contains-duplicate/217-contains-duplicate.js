4de61123de47349fd42a2b5f893c920dd1fd6c68
Ricky Mussig <30369020+MussigSolutions@users.noreply.github.com>
2022-04-27 14:06:19 +1000

    Time: 144 ms (36.67%), Space: 50.5 MB (76.42%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -4,6 +4,9 @@
  */
 var containsDuplicate = function(nums) {
   
+  //Check if amount in the set is different to the amount input
+  
+  //Set.size will not count duplicate values
   
   if(new Set(nums).size < nums.length){
     
