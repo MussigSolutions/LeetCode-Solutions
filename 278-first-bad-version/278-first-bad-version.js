eaac5931f3d4b746160275b45ae0d807e933e642
Ricky Mussig <30369020+NotRychurd@users.noreply.github.com>
2022-04-27 12:45:10 +1000

    Time: 105 ms (17.95%), Space: 41.9 MB (64.10%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.js
+++ 278-first-bad-version/278-first-bad-version.js
@@ -23,21 +23,23 @@ var solution = function(isBadVersion) {
       
     var l = 0, r = n;
 
-      
+      //Checks to see if n = first bad version
       if(isBadVersion(r)&&!isBadVersion(r-1)){
         return r;
       }while((r-l)>=1){    
         
       var mid = Math.floor (l+(r-l)/2);
-        
+        //Check if current var (isBadVersion && var-1!isBadVersion)
       if(isBadVersion(mid)){
         if(!isBadVersion(mid-1)) return mid;
+        
+        //If true we recognise the first bad version is in the first half of versions
         r = mid;
         
       }else{ 
         
-        if(!isBadVersion(mid))
-         
+
+
         l = mid; 
       }
         
