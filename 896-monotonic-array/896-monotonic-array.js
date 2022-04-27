4f70759377a77a8361f96220f0f3be9b15248d07
Ricky Mussig <30369020+MussigSolutions@users.noreply.github.com>
2022-04-27 14:36:52 +1000

    Time: 141 ms (30.89%), Space: 53.3 MB (73.56%) - LeetHub

--- 896-monotonic-array/896-monotonic-array.js
+++ 896-monotonic-array/896-monotonic-array.js
@@ -8,8 +8,8 @@ var isMonotonic = function(nums) {
   var increasing = true, decreasing = true;
   
         for (let i = 1; i < nums.length; ++i) {
-            increasing = increasing && nums[i-1] <= nums[i];
-            decreasing = decreasing && nums[i-1] >= nums[i];
+            increasing = increasing & nums[i-1] <= nums[i];
+            decreasing = decreasing & nums[i-1] >= nums[i];
         }
         return increasing || decreasing;
     
