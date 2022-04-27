b25627796dc6a20b90928acf51d771adddc2e5d2
Ricky Mussig <30369020+MussigSolutions@users.noreply.github.com>
2022-04-27 14:58:49 +1000

    Time: 118 ms (51.44%), Space: 53.6 MB (31.03%) - LeetHub

--- 896-monotonic-array/896-monotonic-array.js
+++ 896-monotonic-array/896-monotonic-array.js
@@ -9,9 +9,9 @@ var isMonotonic = function(nums) {
   
         for (let i = 1; i < nums.length; ++i) {
           
-          //if current calc is true check the next num[i] for monotonic increasing else move on to check for monotonic decreasing
+          //if current calc is true check if current value is smaller than next, if not array is not monotonic increasing.
             if((increasing==true) && !(nums[i-1] <= nums[i])) increasing = false;
-          //Check if current value is smaller than next, if false
+          //Check if current value is larger than next, if not array is not monotonic decreasing.
             if(!(nums[i-1] >= nums[i])) decreasing = false;
           
 
