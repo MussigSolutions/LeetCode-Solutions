154711321977b923e6b41f7ed18c20fba572b1b6
Ricky Mussig <30369020+NotRychurd@users.noreply.github.com>
2022-04-27 11:41:05 +1000

    Time: 65 ms (87.38%), Space: 45.1 MB (64.40%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -5,9 +5,17 @@
  */
 var search = function(nums, target) {
   
+  //checking if array includes target
+  
   if(nums.includes(target)){
+    
+    //true returns the position of target
+    
     return nums.indexOf(target);   
   }else
+    
+    //false returns -1
+    
     return -1;
   
 };
