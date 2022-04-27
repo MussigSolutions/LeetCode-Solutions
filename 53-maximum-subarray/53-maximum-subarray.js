c6b005f9bec2379c1546e80585837c77479c675a
Ricky Mussig <30369020+MussigSolutions@users.noreply.github.com>
2022-04-27 14:22:22 +1000

    Time: 136 ms (19.09%), Space: 50.3 MB (73.73%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.js
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -8,7 +8,12 @@ var maxSubArray = function(nums) {
   
     for (var i=1;i<nums.length;++i){
       
+      //Checks if the sum of the currently stored value + next num is higher than the next num alone.
+      //Could possibly be faster to check if divisible by 0?
     	possibleMax = Math.max(possibleMax+nums[i],nums[i]);
+      
+      //compares the current highest value to the possibly highest value
+            
     	currentMax = Math.max(currentMax, possibleMax);	
       
     }
